dee8e27b67e817da0cb93f835034438e95a91eda
mustafahawa004 <mustafahawa004@gmail.com>
2024-02-03 18:44:46 -0500

    Logo has been added

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,10 +3,15 @@ import React from 'react';
 import './App.css';
 import FileUpload from './components/FileUpload';
 
+const logoPath = '/logo/logo'; // Base path for logo files (update based on your project structure)
+
+const getLogoPath = (fileType) => `${logoPath}.${fileType}`;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={getLogoPath('png')} alt="Logo" className="App-logo" />
         <FileUpload />
       </header>
     </div>
