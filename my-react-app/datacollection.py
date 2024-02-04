c2dd2b6c93a0ed8bc87e1cf15e042e5c05c28461
mustafahawa004 <mustafahawa004@gmail.com>
2024-02-04 05:58:02 -0500

    plan b

---
+++ my-react-app/datacollection.py
@@ -0,0 +1,68 @@
+import sys
+import os
+from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+from clarifai_grpc.grpc.api.status import status_code_pb2
+import requests
+
+image_file_path = sys.argv[1]
+
+# Replace 'YOUR_CLARIFAI_API_KEY' with your actual Clarifai API key
+CLARIFAI_API_KEY = 'd301c71a42504160b90ef870c7f410dc'
+
+# Specify the correct user_id/app_id pairings
+# Since you're making inferences outside your app's scope
+PAT = CLARIFAI_API_KEY
+USER_ID = 'clarifai'
+APP_ID = 'main'
+MODEL_ID = 'food-item-recognition'
+MODEL_VERSION_ID = '1d5fd481e0cf4826aa72ec3ff049e044'
+IMAGE_URL = 'https://jesspryles.com/wp-content/uploads/2020/04/untitled-2-1536x960.jpg'
+
+############################################################################
+# YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+############################################################################
+
+channel = ClarifaiChannel.get_grpc_channel()
+stub = service_pb2_grpc.V2Stub(channel)
+metadata = (('authorization', 'Key ' + PAT),)
+userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+
+# Read the image file in binary mode
+with open(image_file_path, "rb") as f:
+    file_bytes = f.read()
+
+post_model_outputs_response = stub.PostModelOutputs(
+    service_pb2.PostModelOutputsRequest(
+        user_app_id=userDataObject,
+        model_id=MODEL_ID,
+        version_id=MODEL_VERSION_ID,
+        inputs=[
+            resources_pb2.Input(
+                data=resources_pb2.Data(
+                    image=resources_pb2.Image(
+                        base64=file_bytes
+                    )
+                )
+            )
+        ]
+    ),
+    metadata=metadata
+)
+
+if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+    print(post_model_outputs_response.status)
+    raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
+
+output = post_model_outputs_response.outputs[0]
+best_concept = max(output.data.concepts, key=lambda x: x.value) if output.data.concepts else None
+if best_concept:
+    query = best_concept.name
+    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
+    response = requests.get(api_url, headers={'X-Api-Key': 'IUWRgqfCgZE9LKNKXfzEKA==G6bLKD0vWAh5GcTW'})
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print("Error:", response.status_code, response.text)
+else:
+    print("No predicted concept available.")
